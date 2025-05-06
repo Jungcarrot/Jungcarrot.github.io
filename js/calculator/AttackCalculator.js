c41ccba9aaedefe855db9f6316bbb442082e8c90
Jungnahyeon <nahyun4669@gmail.com>
2025-05-06 22:09:42 +0900

    Update AttackCalculator.js

--- js/calculator/AttackCalculator.js
+++ js/calculator/AttackCalculator.js
@@ -1,5 +1,23 @@
 export class AttackCalculator {
-  calculate(charLevel) {
-    return 30 * charLevel * Math.pow(1.01, charLevel);
+  constructor(charLevel, petLevel, weaponLevels, skinBuffs) {
+    this.charLevel = charLevel;
+    this.petLevel = petLevel;
+    this.weaponLevels = weaponLevels; // { rod: 10, cannon: 5, ... }
+    this.skinBuffs = skinBuffs;       // { rod: 10, cannon: 5, ... } (percent)
+  }
+
+  calculate() {
+    const base = 30 * this.charLevel * Math.pow(1.01, this.charLevel);
+    const petBonus = this.petLevel * 2;
+
+    let weaponTotal = 0;
+    for (const type in this.weaponLevels) {
+      const level = this.weaponLevels[type];
+      const buff = this.skinBuffs[type] || 0;
+      weaponTotal += level * (1 + buff / 100);
+    }
+
+    return base + petBonus + weaponTotal;
   }
 }
+
